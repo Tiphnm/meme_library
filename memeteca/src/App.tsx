d5883458b6bfe564756053162031a3b2ad085d85
FaroukDev <fa.elayache1@gmail.com>
2021-05-04 23:24:22 +0200

    add button and style in header.js

--- memeteca/src/App.tsx
+++ memeteca/src/App.tsx
@@ -3,6 +3,7 @@ import Header from './components/Header'
 import './App.css';
 import './components/Header.css'
 
+
 function App() {
   return (
     <div className="App">
@@ -10,5 +11,4 @@ function App() {
     </div>
   );
 }
-
 export default App;
--- memeteca/src/components/Header.tsx
+++ memeteca/src/components/Header.tsx
@@ -4,13 +4,20 @@ import meme from '../assets/img/troll.png'
 export default function Header() {
     return (
         <div className="container-header">
-            <header>
-                <div>Hello</div>
-                <div>
-                    <img src={meme} alt="meme-img"/>
-                </div>
                 <div></div>
-            </header>
+                <div className="center-header">
+                    <img src={meme} alt="meme-img"/>
+                    <div className="title-header">
+                        <h1>MEMETECA</h1>
+                    </div>
+                </div>
+                <div className="button-container">
+                    <button className="button button1">UPLOAD
+                    </button>
+                    <button className="button button2">
+                        REGISTER
+                    </button>
+                </div>
         </div>
     )
 }
