fbda78a4c2993e9f2b910e325b2986e23f03829a
Jordandiazfr <jordandiazfr@gmail.com>
2021-05-05 14:56:31 +0200

    Test

---
+++ memeteca-test/src/App.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+  		<div className="login-form">
+			<h1>Register Form</h1>
+			<form action="http://localhost:4000/register" method="POST">
+				<input type="text" name="username" placeholder="Username" required />
+				<input type="password" name="password" placeholder="Password" required />
+				<input type="submit" />
+			</form>
+		</div>
+    </div>
+  );
+}
+
+export default App;
