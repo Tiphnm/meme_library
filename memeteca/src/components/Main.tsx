09a5ff70ec279190d7fc0b1235b865b1c347a2a2
FaroukDev <fa.elayache1@gmail.com>
2021-05-05 13:41:58 +0200

    creation of component aside

--- memeteca/src/components/Main.tsx
+++ memeteca/src/components/Main.tsx
@@ -3,23 +3,36 @@ import meme from '../assets/img/meme1.jpeg'
 
 export default function Main() {
     return (
-        <div className="container-main">
-            <div className="container-meme">
-               <h3 className="title-meme">My Meme</h3>
-                <img src={meme} alt="meme-img"/>
-                <div className="container-comment">
-                <button>
-                    <i className="far fa-thumbs-up fa-3x">
-                    </i>
-                </button>
-                <button>
-                    <i className="far fa-thumbs-down fa-3x">
-                    </i> 
-                </button>
-                    <span>comments -</span>
-                    <span className="span2">313 points</span>
+        <>
+            <div className="container">
+                <div className="container-aside">
+                    <h1>Popular</h1>
+                        <ul>
+                            <li>
+                                <img src="" alt="meme-img"/>
+                            </li>
+                        </ul>
                 </div>
-            </div> 
-        </div>
+                <div className="container-main">
+                    <div className="container-meme">
+                        <h3 className="title-meme">My Meme</h3>
+                        <img src={meme} alt="meme-img" />
+                        <div className="container-comment">
+                            <button>
+                                <i className="far fa-thumbs-up fa-3x">
+                                </i>
+                            </button>
+                            <button>
+                                <i className="far fa-thumbs-down fa-3x">
+                                </i>
+                            </button>
+                            <span>comments -</span>
+                            <span className="span2">313 points</span>
+                        </div>
+                    </div>
+                </div>
+                <div className="container-last"></div>
+            </div>
+        </>
     )
 }
