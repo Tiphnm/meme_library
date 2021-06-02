031706804ebbb7993456ff17fc7ab3dad6e29325
Jordandiazfr <jordandiazfr@gmail.com>
2021-06-02 16:47:14 +0200

    meme images are now sorted by last and the last img is the first in the homepage

--- memeteca/src/pages/Hotmemes.tsx
+++ memeteca/src/pages/Hotmemes.tsx
@@ -10,23 +10,22 @@ import loaderBlack from "../assets/img/loaderblack.gif"
 
 /* Env vars */ 
 dotenv.config()
-const noenv: string = "CONFIGURE YOUR ENV VARS"
-const environment = process.env.REACT_APP_ENV || noenv
-const api_dev = process.env.REACT_APP_API_DEV || noenv
-const api_prod ="https://memetecapi.azurewebsites.net/"
+
+const api_dev = process.env.REACT_APP_API_DEV || "CONFIGURE YOUR ENV VARS"
+const api_prod =  process.env.REACT_APP_API_PROD
 
 /* If the ENV environment is active then our api is in Localhost */ 
-let api_url: string 
-environment == "DEV"? (api_url = api_dev) : (api_url = api_prod)
+const api_url = process.env.NODE_ENV == "development"?  api_dev : api_prod
 
 
 export default function HotMemes () {
+    console.log(process.env.NODE_ENV)
     const [loadingMemes, setLoadingMemes] = useState(true)
     const [memes, setMemes] = useState<newMeme[]>()
     
     useEffect(() => {
         async function getData()  {
-            await axios.get(api_prod+"/memes").then( (res) => { /* api_url */ 
+            await axios.get(api_url+"/memes").then( (res) => { /* api_url */ 
                 // Check if the memes arrived
                 console.log(res.data)
                 setMemes(res.data)
@@ -42,6 +41,7 @@ export default function HotMemes () {
     return (
         <div >
               {console.log(loadingMemes)} 
+              {console.log(memes)}
            {loadingMemes? <img className="hotmeme" alt="loading..." src={loaderBlack} ></img>: 
             memes!.map(meme => <Meme data={meme} key={meme._id}/>)}
             {console.log(memes)}
